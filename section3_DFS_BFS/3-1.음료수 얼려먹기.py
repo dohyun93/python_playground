7ce9816117500e92309af4fc4eb3a5f2569e8db4
dohyun93 <kwondohyun93@naver.com>
2021-06-17 08:27:48 +0900

    Python playground - (3). DFS/BFS   3. DFS와 BFS   DFS: 재귀, 스택 (append, pop 충분)   BFS: 넓이우선탐색, 큐 (deque)

--- section3_DFS_BFS/3-1.음료수 얼려먹기.py
+++ section3_DFS_BFS/3-1.음료수 얼려먹기.py
@@ -1,9 +1,30 @@
-def recursive(i):
-    print(i, "번째 호출")
-    if i == 10:
-        print(i, "번째 호출 종료")
-        return
-    recursive(i+1)
-    print(i, "번째 호출 종료")
+from collections import deque
 
-recursive(1)
+n, m = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+dr = [0, 0, 1, -1]
+dc = [1, -1, 0, 0]
+
+def bfs(r, c):
+    dq = deque([(r, c)])
+    while dq:
+        r, c = dq.popleft()
+        graph[r][c] = 1 # 방문.
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < n and 0 <= nc < m:
+                if graph[nr][nc] == 0:
+                    dq.append((nr, nc))
+                    graph[nr][nc] = 1
+
+answer = 0
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 0:
+            answer += 1
+            bfs(i, j)
+print(answer)
