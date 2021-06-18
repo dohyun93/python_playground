5d4721d63496dca0c37fee7639e7651a00b030b1
dohyun93 <kwondohyun93@naver.com>
2021-06-19 07:48:39 +0900

    Python playground - (3). DFS/BFS   3. DFS와 BFS   DFS: 재귀, 스택 (append, pop 충분)   BFS: 넓이우선탐색, 큐 (deque)

--- section3_DFS_BFS/3-1.음료수 얼려먹기.py
+++ section3_DFS_BFS/3-1.음료수 얼려먹기.py
@@ -8,6 +8,16 @@ visited = [[0]*m for _ in range(n)]
 dr = [0, 0, 1, -1]
 dc = [1, -1, 0, 0]
 
+def dfs(i, j):
+    visited[i][j] = 1
+    for k in range(4):
+        nr = i + dr[k]
+        nc = j + dc[k]
+        if 0 <= nr < n and 0 <= nc < m:
+            if graph[nr][nc] == 0:
+                if visited[nr][nc] == 0:
+                    dfs(nr, nc)
+
 def bfs(r, c):
     dq = deque([(r, c)])
     visited[r][c] = 1
@@ -28,6 +38,7 @@ for i in range(n):
     for j in range(m):
         if graph[i][j] == 0 and visited[i][j] == 0:
             answer += 1
-            bfs(i, j)
+            #bfs(i, j)
+            dfs(i, j)
 
 print(answer)
