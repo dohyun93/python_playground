b046674d28593bda985f057c29e43f5705c6853a
dohyun93 <kwondohyun93@naver.com>
2021-06-21 23:19:11 +0900

    Python playground - (8). 동적프로그래밍   8. Dynamic programming   - 메모이제이션 -> 공간효율화는 떨어지지만, 연산효율성은 증대된다.   - 반복적인 재귀 함수 호출을 통해 비효율적 연산하지 않고, 이미 연산한 것은 기억.   - 점화식 기반의 프로그래밍을 한다.

--- section6_다이나믹 프로그래밍/6-1.피보나치수열.py
+++ section6_다이나믹 프로그래밍/6-1.피보나치수열.py
@@ -10,15 +10,28 @@ def fibo_bad(x):
 fibo = [0] * 100
 fibo[1], fibo[2] = 1, 1
 
-def fibo_good(x):
+def fibo_topdown(x):
     if x <= 0:
         return -1
     elif 0 < x <= 2:
         return fibo[x]
     elif fibo[x] != 0:
         return fibo[x]
-    fibo[x] = fibo_good(x-1) + fibo_good(x-2)
+    fibo[x] = fibo_topdown(x-1) + fibo_topdown(x-2)
     return fibo[x]
+
+def fibo_botomup(x):
+    if x <= 0:
+        return -1
+    elif 0 < x <= 2:
+        return fibo[x]
+    else:
+        if fibo[x] != 0:
+            return fibo[x]
+
+        for i in range(3, x+1):
+            fibo[i] = fibo(i-1) + fibo(i-2)
+        return fibo[x]
 
 x = int(input())
 
@@ -28,6 +41,11 @@ bad_end = time.time()
 print(f"bad fibo took {bad_end - bad_start} ms")
 
 good_start = time.time()
-print(fibo_good(x))
+print(fibo_topdown(x))
 good_end = time.time()
 print(f"good fibo took {good_end - good_start} ms")
+
+good_start = time.time()
+print(fibo_botomup(x))
+good_end = time.time()
+print(f"fibo botom up took {good_end - good_start} ms")
