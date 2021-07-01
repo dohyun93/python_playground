a95986272c01eaeb7b38d1178ed471c72b9ab9a1
dohyun93 <kwondohyun93@naver.com>
2021-07-02 00:03:30 +0900

    Python playground - (10). 그래프 이론   10. 그래프 이론   - 크루스칼 알고리즘 O(ElogE)   - 위상정렬 알고리즘 O(V + E)   - 서로소 집합 자료구조

--- section8_그래프 이론/8-1.서로소집합.py
+++ section8_그래프 이론/8-1.서로소집합.py
@@ -1,16 +1,20 @@
-def findParent(parentList, x):
+# V번의 탐색, M번의 find 혹은 union연산시 O(VM)
+def findRootBad(parentList, x): # 루트노드를 매번 찾기만 함.
     if x != parentList[x]:
-        return findParent(parentList, parentList[x])
+        return findRootBad(parentList, parentList[x])
     return x
 
-def findRoot(parentList, x):
+# 부모리스트 갱신
+# 바로 루트노드로 부모 정보를 갱신을 해주기 때문에, 더욱 효율적이다.
+# findParent나 findRoot나 결국 루트노드를 탐색한다.
+def findRootGood(parentList, x):
     if x != parentList[x]:
-        parentList[x] = findRoot(parentList, parentList[x])
+        parentList[x] = findRootGood(parentList, parentList[x])
     return parentList[x]
 
 def union_parent(parentList, a, b):
-    aParent = findRoot(parentList, a)
-    bParent = findRoot(parentList, b)
+    aParent = findRootGood(parentList, a)
+    bParent = findRootGood(parentList, b)
     if aParent < bParent:
         parentList[b] = aParent
     else:
@@ -28,7 +32,7 @@ for i in range(e):
 
 print("각 원소가 속한 집합(root): ")
 for i in range(1, v+1):
-    print(findRoot(parentList, i), end=' ')
+    print(findRootGood(parentList, i), end=' ')
 
 print("각 원소의 부모: ")
 for i in range(1, v+1):
