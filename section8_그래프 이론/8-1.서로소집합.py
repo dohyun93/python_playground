c10ce513d69fb187b136ed506ba126e0b77afd8f
dohyun93 <kwondohyun93@naver.com>
2021-07-03 08:19:04 +0900

    Python playground - (10). 그래프 이론   10. 그래프 이론   - 크루스칼 알고리즘 O(ElogE)   - 위상정렬 알고리즘 O(V + E)   - 서로소 집합 자료구조

--- section8_그래프 이론/8-1.서로소집합.py
+++ section8_그래프 이론/8-1.서로소집합.py
@@ -1,39 +1,43 @@
-# V번의 탐색, M번의 find 혹은 union연산시 O(VM)
-def findRootBad(parentList, x): # 루트노드를 매번 찾기만 함.
-    if x != parentList[x]:
-        return findRootBad(parentList, parentList[x])
-    return x
+def findRoot_1(parent, x):
+    if parent[x] != x:
+        return findRoot_1(parent, parent[x])
+    return parent[x]
 
-# 부모리스트 갱신
-# 바로 루트노드로 부모 정보를 갱신을 해주기 때문에, 더욱 효율적이다.
-# findParent나 findRoot나 결국 루트노드를 탐색한다.
-def findRootGood(parentList, x):
-    if x != parentList[x]:
-        parentList[x] = findRootGood(parentList, parentList[x])
-    return parentList[x]
+# 경로 압축. 부모 테이블에 루트정보를 넣음.
+# 그래서 위 함수에서 부모테이블의 정보와 다름. (부모 테이블(위) vs 루트 테이블(아래))
+def findRoot_2(parent, x):
+    if parent[x] != x:
+        parent[x] = findRoot_2(parent, parent[x])
+    return parent[x]
 
-def union_parent(parentList, a, b):
-    aParent = findRootGood(parentList, a)
-    bParent = findRootGood(parentList, b)
-    if aParent < bParent:
-        parentList[b] = aParent
+# findRoot_1은 루트노드를 찾을 수는 있으나, 부모 정보를 갱신해주지는 않음.
+
+def makeUnion(parent, a, b):
+    # makeUnion은 두 노드의 루트노드를 비교해서 더 작은 루트노드를 다른 노드의 부모로 지정한다.
+    # 루트노드를 그룹으로 생각하고 그루핑을 한다고 생각하면 편함.
+    aRoot = findRoot_2(parent, a)
+    bRoot = findRoot_2(parent, b)
+    if aRoot < bRoot:
+        parent[b] = aRoot
     else:
-        parentList[a] = bParent
+        parent[a] = bRoot
+
 
 v, e = map(int, input().split())
-parentList = [0] * (1+v)
+parent = [0] * (v+1)
 
 for i in range(1, v+1):
-    parentList[i] = i
+    parent[i] = i
 
 for i in range(e):
     a, b = map(int, input().split())
-    union_parent(parentList, a, b)
+    makeUnion(parent, a, b)
 
-print("각 원소가 속한 집합(root): ")
+print('각 원소의 루트노드')
 for i in range(1, v+1):
-    print(findRootGood(parentList, i), end=' ')
+    #print(findRoot_1(parent, i), end=' ')
+    print(findRoot_2(parent, i), end=' ')
 
-print("각 원소의 부모: ")
+print('\n각 원소의 부모노드')
 for i in range(1, v+1):
-    print(parentList[i], end=' ')
+    print(parent[i], end= ' ')
