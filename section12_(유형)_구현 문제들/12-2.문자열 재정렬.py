051accb0739dabbba997910458ad248fe173f7a0
dohyun93 <kwondohyun93@naver.com>
2021-07-05 12:22:27 +0900

    Python playground - (12). 유형별 풀이 - 구현   12-1. 럭키 스트레이트

---
+++ section12_(유형)_구현 문제들/12-2.문자열 재정렬.py
@@ -0,0 +1,21 @@
+# 알파벳 대문자와 숫자(0~9) 로만 구성된 문자열 입력
+# 모든 알파벳을 오름차순으로 정렬해서 이어서 출력하고, 그 뒤에 모든 숫자를 더한 값을 이어서 출력하라.
+
+S = input()
+
+character = []
+number = []
+
+for i in S:
+    if 'A' <= i <= 'Z':
+        character.append(i)
+    else:
+        number.append(int(i))
+
+character.sort()
+
+answer = []
+answer = ''.join(character)
+answer += str(sum(number))
+
+print(answer)
