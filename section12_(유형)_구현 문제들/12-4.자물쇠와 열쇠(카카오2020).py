f0f7a8b1eb1d1c217278ba02f7bbf5be1c867bb2
dohyun93 <kwondohyun93@naver.com>
2021-07-08 08:15:39 +0900

    Python playground - (12). 유형별 풀이 - 구현   12-4. 자물쇠와 열쇠

--- section12_(유형)_구현 문제들/12-4.자물쇠와 열쇠(카카오2020).py
+++ section12_(유형)_구현 문제들/12-4.자물쇠와 열쇠(카카오2020).py
@@ -1,46 +1,49 @@
 def rotate(key):
-    M = len(key)
-    rotatedKey = [[0] * M for _ in range(M)]
-    for r in range(M):
-        for c in range(M):
-            rotatedKey[c][M-r-1] = key[r][c]
-    return rotatedKey
+    m = len(key)
+    key_ = [[0]*m for _ in range(m)]
+    for r in range(m):
+        for c in range(m):
+            key_[c][m-r-1] = key[r][c]
+    return key_
 
 def check(lock_padding):
-    lock_length = len(lock_padding) // 3
-    for i in range(lock_length, lock_length*2):
-        for j in range(lock_length, lock_length*2):
-            if lock_padding[i][j] != 1:
+    n = len(lock_padding) // 3
+    for r in range(n, n*2):
+        for c in range(n, n*2):
+            if lock_padding[r][c] != 1:
                 return False
     return True
 
-#키를 자물쇠에 넣었을때
-def keyIn(lock_padding, key):
+# 자물쇠에 열쇠 넣기
+def KeyIn(lock_padding, key):
     n = len(lock_padding) // 3
-    for x in range(n*2):
-        for y in range(n*2):
+    # r, c는 비교 시작할 lock_padding의 위치
+    # i, j는 r, c로부터 시작할 key요소의 위치
+    for r in range(n*2):
+        for c in range(n*2):
             for i in range(len(key)):
                 for j in range(len(key)):
-                    lock_padding[x + i][y + j] += key[i][j]
-            if check(lock_padding) == True:
+                    lock_padding[r+i][c+j] += key[i][j]
+            if check(lock_padding):
                 return True
             for i in range(len(key)):
                 for j in range(len(key)):
-                    lock_padding[x + i][y + j] -= key[i][j]
+                    lock_padding[r + i][c + j] -= key[i][j]
     return False
 
 def solution(key, lock):
+    answer = False
     n = len(lock)
-    rotate_count = 0
-    lock_padding = [[0] * (n*3) for _ in range(n*3)] #새로운 자물쇠 만들기
-    for i in range(n):
-        for j in range(n):
-            lock_padding[i + n][j + n] = lock[i][j]
+    lock_padding = [[0] * (n*3) for _ in range(3*n)]
+    for r in range(n):
+        for c in range(n):
+            lock_padding[r+n][c+n] = lock[r][c]
+    rotateCount = 0
     while True:
-        answer = keyIn(lock_padding, key)
-        if answer or rotate_count == 3:
+        answer = KeyIn(lock_padding, key)
+        if answer or rotateCount == 3:
             break
         else:
+            rotateCount += 1
             key = rotate(key)
-            rotate_count += 1
     return answer
