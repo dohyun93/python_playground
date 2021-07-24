ea828d0c41b61bb2dcca438f38595665f5e355e3
dohyun93 <kwondohyun93@naver.com>
2021-07-24 23:36:46 +0900

    section16 - 다이나믹 프로그래밍     16-6. 편집 거리

--- section16_(유형)_다이나믹프로그래밍/16-5.못생긴 수(Google인터뷰).py
+++ section16_(유형)_다이나믹프로그래밍/16-5.못생긴 수(Google인터뷰).py
@@ -2,24 +2,26 @@
 # 1은 못생긴 수라고 할 때 n번째 못생긴 수를 구하시오.
 
 n = int(input())
-ugly = [0] * n
-ugly[0] = 1
-
-idx2, idx3, idx5 = 0, 0, 0 # 곱해야 할 각 수(2, 3, 5)의 피연산 인덱스.
+idx2, idx3, idx5 = 0, 0, 0
 next2, next3, next5 = 2, 3, 5
 
-for i in range(1, n):
-    ugly[i] = min(next2, next3, next5)
-    if ugly[i] == next2:
+dp = [0] * n
+dp[0] = 1
+
+for idx in range(1, n):
+    dp[idx] = min(next2, next3, next5)
+    if dp[idx] == next2:
+        print("idx, 조건1", idx)
         idx2 += 1
-        next2 = ugly[idx2] * 2
-    if ugly[i] == next3:
+        next2 = dp[idx2] * 2
+    if dp[idx] == next3:
+        print("idx, 조건2", idx)
         idx3 += 1
-        next3 = ugly[idx3] * 3
-    if ugly[i] == next5:
+        next3 = dp[idx3] * 3
+    if dp[idx] == next5:
+        print("idx, 조건3", idx)
         idx5 += 1
-        next5 = ugly[idx5] * 5
+        next5 = dp[idx5] * 5
 
-print(ugly)
 
-print(ugly[n-1])
+print(dp[n-1])
