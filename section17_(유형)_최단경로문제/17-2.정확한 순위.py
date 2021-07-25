6fa15c613e5ed095dd8aee0b2476eb7dfcb3f898
dohyun93 <kwondohyun93@naver.com>
2021-07-25 23:40:08 +0900

    section17 - 최단 거리     17-2. 정확한 순위 (플로이드 워셜)

---
+++ section17_(유형)_최단경로문제/17-2.정확한 순위.py
@@ -0,0 +1,33 @@
+import sys
+
+# 1. 기본정보 입력 및 그래프 초기화.
+# Note that node : 1 ~ N.
+N, M = map(int, sys.stdin.readline().rsplit())
+INF = int(1e9)
+graph = [[INF] * (N+1) for _ in range(N+1)]
+for m in range(M):
+    src, dst = map(int, sys.stdin.readline().rsplit())
+    graph[src][dst] = 1
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if i == j:
+            graph[i][j] = 0
+
+# 2. 플로이드 워셜 알고리즘 (O(N^3))
+for k in range(1, N+1):
+    for src in range(1, N+1):
+        for dst in range(1, N+1):
+            graph[src][dst] = min(graph[src][dst], graph[src][k] + graph[k][dst])
+
+# 3. node 잡고, 이 노드로 가는 경로가 있는 것(graph[src][node] != 0)의 수 +
+#    이 노드에서 도달가능 경로의 노드의수 (graph[node][dst]) 결과 (간선 수) 가 N-1이면 정답 +1.
+answer = 0
+for node1 in range(1, N+1):
+    cnt = 0
+    for node2 in range(1, N+1):
+        if graph[node1][node2] != INF or graph[node2][node1] != INF:
+           cnt += 1
+    if cnt == N:
+        answer += 1
+print(answer)
