1409f30e1ad8aad976272fb3892f13853f3410ea
dohyun93 <kwondohyun93@naver.com>
2021-08-14 00:08:02 +0900

    2019 KAKAO BLIND RECRUITMENT 후보키 - 조합, 구현

--- 2019 KAKAO BLIND RECRUITMENT/후보키_8월10일.py
+++ 2019 KAKAO BLIND RECRUITMENT/후보키_8월10일.py
@@ -1,44 +1,41 @@
 from itertools import combinations
 
+
 def solution(relation):
-    answer = 0
-    # 문제 풀이 전략
-    # 1. 컬럼 데이터들을 따로 뽑아둔다.
-    # 2. 조합으로 선택할 열들을 고른다. (1개 조합 ~ 컬럼의 개수 조합)
-    # 3. 만약 3개 (0, 2, 4 idx의 열)를 골랐다면 해당 열의 데이터들을 1번을 활용해
-    # 리스트에 다 넣어주고 (페어로), 그 개수가 집합으로 변경한 것과 동일한지 계산한다.
-    # 만약 가능하다면 answer +1.
-    colNum = len(relation[0])
-    colData = [[] for _ in range(colNum)]
-    for tuple in relation:
-        for j in range(len(tuple)):
-            colData[j].append(tuple[j])
-    print(colData)
+    # 컬럼의 개수, 행의 개수, 컬럼 리스트(인덱스로 표현함)
+    n_col = len(relation[0])
+    n_row = len(relation)
+    list_col = [i for i in range(n_col)]
+    # 컬럼 리스트로 후보키 조합 만들기
+    # 주의사항 : list를 [] 없이 추가하고 싶을 때는 append 대신 extend 사용!
+    combs_col = []
+    for i in range(1, n_col + 1):
+        combs_col.extend(list(combinations(list_col, i)))
+    # print(combs_col)
 
-    colidx = [i for i in range(colNum)]
-    for selection in range(1, colNum+1):
-        # 1. 1개부터 컬럼전체 까지
-        # 조사한 조합이 유일성을 만족하는 경우, 정답의 조합들 in 현재 조합이라면 추가하지 않음.
-        # 아니라면 추가.
-        for colCombi in list(combinations(colidx, selection)):
-            # 2. selection개수만큼 조합 뽑기
-            print(colCombi)
+    # 유일성 : 유일하게 식별되어야 함 =&gt; 해당 컬럼 안에 중복된 값이 없어야 한다.
+    uniques = []
+    for i in combs_col:
+        key = set()  # key : {} 주의 : append 대신 add를 이용
+        for j in range(n_row):
+            key.add(tuple(relation[j][k] for k in i))  # set을 이용하므로 tuple 사용
+            # print(key)
+        if len(key) == n_row: uniques.append(i)
+    # print(uniques)
 
-
-
-    return answer
+    answer = []
+    # 최소성 : uniques에 포함된 부분집합 안에 부분집합이 포함되면 안됨.
+    for i in uniques:
+        sub = set(i)
+        # print(sub)
+        check = True
+        for j in answer:
+            if j.issubset(sub):  # answer에 들어가있는 부분집합이 sub 안에 포함될 경우, sub 미포함.
+                check = False
+        if check == True: answer.append(sub)
+    # print(answer)
+    return len(answer)
 
 relation = [["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]
-solution(relation)
-
-colidx = [i for i in range(0, 4)]
-for combi in range(1, 4):
-    curCombi = list(combinations(colidx, combi))
-    if combi == 1:
-        print("1일떄: ", curCombi)
-        print(curCombi)
-        print(curCombi[0][0])
-        print(curCombi[1][0])
-        print(curCombi[2][0])
-        print(curCombi[3][0])
-
+result = solution(relation)
+print(result)
