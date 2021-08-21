247de37cfad460d6e4e50aa0e62646ff77e541ef
dohyun93 <kwondohyun93@naver.com>
2021-08-21 18:03:37 +0900

    2021 카카오 블라인드 코딩테스트 다익스트라 알고리즘. - 합승 택시 요금

--- 2021 KAKAO BLIND RECRUITMENT/합승 택시 요금_8월 16일.py
+++ 2021 KAKAO BLIND RECRUITMENT/합승 택시 요금_8월 16일.py
@@ -1,33 +1,63 @@
 # https://programmers.co.kr/learn/courses/30/lessons/72413
 # 다익스트라 알고리즘을 이용한 최단거리 문제 풀이.
 
-import heapq
-
+from heapq import heappush, heappop
 INF = int(1e9)
 
 def dijkstra(graph, start):
-    pq = [(0, start)]
-    distance = [INF] * len(graph)
+    n = len(graph)
+    distance = [INF] * (n+1)
+    q = [(0, start)] # 비용, start지점
     distance[start] = 0
-
-    while pq:
-        cost, point = heapq.heappop(pq)
-        if distance[point] < cost:
+    while q:
+        cumulCost, curPos = heappop(q)
+        if cumulCost > distance[curPos]:
             continue
-        for dst, fare in graph[point]:
-            fare += cost
-            if fare < distance[dst]:
-                distance[dst] = fare
-                heapq.heappush(pq, (fare, dst))
+        else:
+            for dst, fare in enumerate(graph[curPos]): ### 아래와 다름
+                if cumulCost + fare < distance[dst]:
+                    distance[dst] = cumulCost + fare
+                    heappush(q, (distance[dst], dst))
     return distance
 
 def solution(n, s, a, b, fares):
-    graph = [[] for _ in range(n+1)]
     answer = INF
-    for src, dst, fare in fares:
-        graph[src].append((dst, fare))
-        graph[dst].append((src, fare))
-    for k in range(1, n+1):
-        dist_k = dijkstra(graph, k)
-        answer = min(answer, dist_k[s] + dist_k[a] + dist_k[b])
+    graph = [[INF] * (n+1) for _ in range(1+n)]
+    for src, dst, cost in fares:
+        graph[src][dst] = cost ### 아래와 다름
+        graph[dst][src] = cost
+    for start in range(1, n+1):
+        distance = dijkstra(graph, start)
+        answer = min(distance[s] + distance[a] + distance[b], answer)
     return answer
+#
+# import heapq
+#
+# INF = int(1e9)
+#
+# def dijkstra(graph, start):
+#     pq = [(0, start)]
+#     distance = [INF] * len(graph)
+#     distance[start] = 0
+#
+#     while pq:
+#         cost, point = heapq.heappop(pq)
+#         if distance[point] < cost:
+#             continue
+#         for dst, fare in graph[point]:
+#             fare += cost
+#             if fare < distance[dst]:
+#                 distance[dst] = fare
+#                 heapq.heappush(pq, (fare, dst))
+#     return distance
+#
+# def solution(n, s, a, b, fares):
+#     graph = [[] for _ in range(n+1)]
+#     answer = INF
+#     for src, dst, fare in fares:
+#         graph[src].append((dst, fare))
+#         graph[dst].append((src, fare))
+#     for k in range(1, n+1):
+#         dist_k = dijkstra(graph, k)
+#         answer = min(answer, dist_k[s] + dist_k[a] + dist_k[b])
+#     return answer
